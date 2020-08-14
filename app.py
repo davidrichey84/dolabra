55e3cd4b73f0a2640579a5e0b7609abc4427aa15
David Richey <drichey@us.ibm.com>
2020-08-14 00:13:09 -0500

    got argument parsing working a bit better

--- app.py
+++ app.py
@@ -17,35 +17,44 @@ def setup_logging(level='DEBUG'):
 
 def setup_arg_parser():
     parser = ArgumentParser()
-    parser.add_argument("-k", "--kafka", action='store_true', help="Kafka message queue mode", required=False)
-    parser.add_argument("-n", "--nats", action='store_true', help="Nats message queue mode", required=False)
-    parser.add_argument("-t", "--target", action='store', dest='target_server', help="Target kafka/NATS server:port", required=False)
-    parser.add_argument("-st", "--subtopic", action='store', dest='sub_topic', help="Subscription topic you would like to subscribe to", required=False)
-    parser.add_argument("-pt", "--pubtopic", action='store', dest='pub_topic', help="Publish topic you would like to publish results to", required=False)
-    parser.add_argument("-r", "--restapi", action='store_true', help="REST API mode", required=False)
-    parser.add_argument("-b", "--bind", action='store', dest='bind', help="Interface and port to bind REST API. ex: 0.0.0.0:5000", required=False)
-    parser.add_argument("-w", "--workers", action='store', dest='gun_workers', help="Number of gunicorn workers for REST API", required=False)
-    return parser
+    subparsers = parser.add_subparsers()
+    return parser, subparsers
 
-def parse_args():
-    if not args.kafka and not args.nats and not args.restapi:
-        print("No run method option specified. Please choose either (-k)Kafka, (-n)Nats or (-r)REST")
-        sys.exit(1)
-    else:
-        if args.kafka:
-            if args.target_server and args.sub_topic and args.pub_topic:
-                print("Sweet! Kafka mode initialized")
-                kafka_ner.KafkaNER(args)
-            else:
-                print("No target, sub topic or pub topic")
-                sys.exit(1)
-        elif args.nats:
-            if args.target_server and args.sub_topic and args.pub_topic:
-                print("Not implemented yet")
-                sys.exit(1)
-        elif args.restapi:
-            rest_ner.REST(args)
+def setup_kafka_subparser(subparsers):
+    parser_kafka = subparsers.add_parser('kafka')
+    parser_kafka.add_argument('-t', '--target', action='store', dest='target', help='target:port', required=True)
+    parser_kafka.add_argument('-st', '--subtopic', action='store', dest='sub_topic', help='Subscription topic', required=True)
+    parser_kafka.add_argument('-pt', '--pubtopic', action='store', dest='pub_topic', help='Publish topic', required=True)
+    return subparsers, parser_kafka
+
+def setup_nats_subparser(subparsers):
+    parser_nats = subparsers.add_parser('nats')
+    parser_nats.add_argument('-t', '--target', action='store', dest='target', help='target:port', required=True)
+    parser_nats.add_argument('-st', '--subtopic', action='store', dest='sub_topic', help='subscription topic', required=True)
+    parser_nats.add_argument('-pt', '--pubtopic', action='store', dest='pub_topic', help='publish topic', required=True)
+    return subparsers, parser_nats
+
+def setup_rest_subparser(subparsers):
+    parser_rest = subparsers.add_parser('rest')
+    parser_rest.add_argument('-b', '--bind', action='store', dest='bind', help='Interface and port to bind REST API. ex: 0.0.0.0:5000', required=True)
+    parser_rest.add_argument('-w', '--workers', action='store', dest='gun_workers', help='Number of gunicorn workers for REST API', required=True)
+    return subparsers, parser_rest
+
+def kafka(args):
+    kafka_ner.KafkaNER(args)
+
+def nats(args):
+    print("This feature has not yet been implemented...")
+    sys.exit(1)
+
+def rest(args):
+    rest_ner.REST(args)
+
+
 if __name__ == '__main__':
-    parser = setup_arg_parser()
-    args = parser.parse_args()
-    parse_args()
+    parser, subparsers = setup_arg_parser()
+    subparsers, parser_kafka = setup_kafka_subparser(subparsers)
+    # Not yet implemented
+    #subparsers, parser_nats = setup_nats_subparser(subparsers)
+    subparsers, parser_rest = setup_rest_subparser(subparsers)
+    parser.parse_args()
